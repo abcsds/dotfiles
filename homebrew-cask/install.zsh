248b2baad209f9c9a0dd26bf4cb09763c2ac9ccb
Alberto Barradas <abcsds@gmail.com>
2015-08-11 14:23:58 -0500

    Add casks

--- homebrew-cask/install.zsh
+++ homebrew-cask/install.zsh
@@ -3,4 +3,31 @@ then
   brew install caskroom/cask/brew-cask
   # install homebrew casks
 
+  # brew cask install alfred
+  brew cask install atom
+  brew cask install virtualbox
+  brew cask install vagrant
+  brew cask install gimp
+  brew cask install google-chrome
+  brew cask install google-drive
+  brew cask install google-hangouts
+  brew cask install google-photos-backup
+  brew cask install grandperspective
+  brew cask install inkscape
+  brew cask install insomniax
+  brew cask install iterm2
+  # brew cask install macvim
+  # brew cask install microsoft-office
+  brew cask install nethackcocoa
+  # brew cask install slack
+  brew cask install vlc
+  brew cask install steam
+  # brew cask install sublime-text
+  brew cask install telegram
+  brew cask install texstudio
+  brew cask install the-unarchiver
+  brew cask install transmission
+  brew cask install tuneup
+  brew cask install unetbootin
+
 fi
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -60,6 +60,7 @@ brew install curl
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
+brew install macvim
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
