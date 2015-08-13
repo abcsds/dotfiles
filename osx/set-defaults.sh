2dce1e59b29a455a254d22e43f2af2f33a316ed9
abcsds <abcsds@gmail.com>
2015-08-12 22:37:25 -0500

    No killall

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -771,10 +771,10 @@ defaults write com.twitter.twitter-mac HideInBackground -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-	"Safari" "SystemUIServer"  \ # "Terminal"
-	"Transmission" "Twitter" "iCal"; do
-	killall "${app}" > /dev/null 2>&1
-done
+# for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+# 	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
+# 	"Safari" "SystemUIServer"  \ # "Terminal"
+# 	"Transmission" "Twitter" "iCal"; do
+# 	killall "${app}" > /dev/null 2>&1
+# done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
