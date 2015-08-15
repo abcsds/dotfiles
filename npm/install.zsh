0be7d73588c7c55131044e12ad931e54ffab8527
abcsds <abcsds@gmail.com>
2015-08-15 01:04:42 -0500

    Update npm script

--- npm/install.zsh
+++ npm/install.zsh
@@ -1,13 +1,11 @@
-if [ -z "$(which npm)" ]
+if [ "$(uname -s)"=="Darwin" ]
 then
-  # Install the Solarized Dark theme for iTerm
-  if [ "$(uname -s)" == "Darwin" ]
+  if [ -z "$(which npm)" ]
   then
     brew install node
   fi
+  npm install npm -g
+  npm update -g
+  npm install -g yo bower grunt-cli gulp
+  npm install -g generator-webapp
 fi
-
-npm install npm -g
-npm update -g
-npm install -g yo bower grunt-cli gulp
-npm install -g generator-webapp
--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,4 +1,3 @@
 alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'
-alias tmux="TERM=xterm-256color tmux"
