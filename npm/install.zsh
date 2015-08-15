71bcc65cab128fbd58fda9454f5a6f1f601ec7d0
abcsds <abcsds@gmail.com>
2015-08-15 14:25:47 -0500

    Add vim NTgit

--- npm/install.zsh
+++ npm/install.zsh
@@ -2,10 +2,10 @@ if [ "$(uname -s)"=="Darwin" ]
 then
   if [ -z "$(which npm)" ]
   then
-    brew install node
+    # brew install node
   fi
-  npm install npm -g
-  npm update -g
-  npm install -g yo bower grunt-cli gulp
-  npm install -g generator-webapp
+  # npm install npm -g
+  # npm update -g
+  # npm install -g yo bower grunt-cli gulp
+  # npm install -g generator-webapp
 fi
