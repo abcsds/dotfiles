89db8285d4fe4b3a1bb5a8c519bef1074804862f
Alberto Barradas <abcsds@gmail.com>
2015-09-13 10:27:21 -0500

    Update cmus config

---
+++ config/config.symlink/cmus/lib.pl
@@ -0,0 +1,120 @@
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/01 - Dime.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/03 - Reality shock.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/04 - Paralo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/05 - Gente reaccion.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/06 - Xeno.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/07 - Asesinos+punk-o.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/08 - Mercenario.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/09 - No te detengas.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/10 - Curame.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/11 - Pecho tierra.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/12 - Centerfold+la rubia y el demonio.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/13 - La cumbia del olvido.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/14 - Cosas del ayer+punk-o.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/15 - Esta noche.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/16 - La dosis perfecta.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/17 - La carencia.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/Panteon rococo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/Panteon rococo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/Curame.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/Curame.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/No te detengas.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/No te detengas.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/Toloache pa`mi negra.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/Toloache pa`mi negra.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/Asesinos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/Asesinos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra-1/Nada paso.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1997]Panteon Rococo- Toloache pa mi negra/Nada paso.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2005]Panteon Rococo- 10 años un panteon muy vivo/02 - La ciudad de la esperanza+malbicho.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/15 - Gracias Compañeros Musicales.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - la rubia y el demonio(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - la dosis perfecta(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - la dosis perfecta224(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteón rococó - la carencia244(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - toloache pa mi negra421(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/02-panteon_rococo-la_ciudad_de_la_esperanza-mil(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - esta noche148(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - curame(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - no title - hotel california (version ska)(2)(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - marcos hall(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - a la izquierda de la tierra - pequeño tratado de un adios14(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - ven, ven ,ven(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - fugaz(2)(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon roccoco - perdon(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/01 - no te c......mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/02 - es tan poco el tiempo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/03 - la dosis perfecta.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/04 - asesinos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/05 - la concha(consentida).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/06 - pecho tierra.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/07 - curame.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/08 - la maquinita.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/09 - marco`s hall.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/10 - pequeño tratado de un adios.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/11 - toloache pa' mi negra.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/12 - nada pasò.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/13 - la bruja.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[1999]Panteon Rococo - A la izquierda de la tierra/14 - borracho ( drunk steady beer).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/01 - Intro.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/02 - La Carencia.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/03 - La Rubia y el Demonio.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/04 - C.D.A..mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/05 - Dime.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/06 - Esta Noche.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/07 - Seguir Bailando.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/08 - Globalizado.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/09 - Perdón.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/10 - Páralo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/11 - Tengo Miedo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/12 - Eres un Mal.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/13 - Gente Reacción.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2002]Panteon Rococo - Compañeros Musicales/14 - Punk-O.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/01.Madre candela.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/02.La ciydad de la esperanza.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/03.Cosas de ayer.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/04.Cumbia del olvido.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/05.Bajo sombras.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/06.1993.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/07.Buscandote.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/08.Freedomland.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/09.Dejando atras.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/10.Fugaz.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/11.Reality shok.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/12.Xeno.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/13.La palomita.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2004]Panteon Rococo - Tres veces tres/14.Programa especial E.Z.L.N..mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/01 Acabame De Matar.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/02 Triste Realidad.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/03 Donde Se Queda.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/04 Vendedora De Caricias.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/05 Estrella Roja.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/06 Que Pasara.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/07 Cuando Tengas.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/08 Caminemos Juntos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/09 Canciones De Amor Y Odio.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/10 No Me Vayas A Dejar.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/11 No Te Recuerdo.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/12 De Luna A Sol.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2007]Panteon Rococo - Panteon Rococo/13 La Distancia.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - panteon rococo - 04 - vendedora de caricias401 - copia(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/01 madre candela(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - estrella roja(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/buscandote(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - che(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - va un mundo raro (tributo a jose alfredo jimenez) - tu recuerdo y yo265(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/01 Salvame.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/02 Cerdoz.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/03 Abajo y a la Izquierda.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/04 Dale Equilibrio.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/05 Jardín de Amapólas.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/06 Democrácia Fecal.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/07 Si, Ya Lo Sé.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/08 Sacúde.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/09 Payaso de Mentiras.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/10 Control Remoto.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/11 Conflictos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/12 Arreglame el Alma.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/[2010]Panteon Rococo - Ejército de Paz/13 Vete Lejos.mp3
+/Users/abcsds/Music/Music/Panteon Rococo/panteon rococo - adelita153(1).mp3
+/Users/abcsds/Music/Music/Panteon Rococo/01 hijo de su(1).mp3
