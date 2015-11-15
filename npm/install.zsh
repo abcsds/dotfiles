0751a7d8cfb7cd7d1d7e6171a59c2073758145be
Alberto Barradas <abcsds@gmail.com>
2015-11-15 17:27:39 -0600

    Enable npm

--- npm/install.zsh
+++ npm/install.zsh
@@ -4,9 +4,9 @@ then
   then
     # brew install node
   fi
-  # npm install npm -g
-  # npm update -g
-  # npm install -g yo bower grunt-cli gulp
-  # npm install -g generator-webapp
+  npm install npm -g
+  npm update -g
+  npm install -g yo bower grunt-cli gulp
+  npm install -g generator-webapp
 fi
 export PATH="$HOME/.node/bin:$PATH"
