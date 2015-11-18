a70e31982bac47e7e471483aa9a293832d35e699
Alberto Barradas <abcsds@gmail.com>
2015-11-18 17:20:44 -0600

    Add mactex

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -1,53 +1,49 @@
-if test "$(uname)" = "Darwin"
-then
-  brew install caskroom/cask/brew-cask
-  # install homebrew casks
-  brew cask install xquartz
-
-  brew cask install acslogo
-  # brew cask install alfred
-  brew cask install atom
-  brew cask install audacity
-  brew cask install bit-slicer
-  brew cask install filezilla
-  # brew cask install ganttproject
-  brew cask install gimp
-  brew cask install github-desktop
-  brew cask install google-chrome
-  brew cask install google-drive
-  brew cask install google-hangouts
-  brew cask install google-photos-backup
-  brew cask install grandperspective
-  brew cask install haskell-platform
-  brew cask install inkscape
-  brew cask install insomniax
-  brew cask install iterm2
-  # brew cask install macvim
-  # brew cask install microsoft-office
-  brew cask install minecraft
-  # brew cask install mongohub
-  brew cask install nethackcocoa
-  brew cask install supercollider
-  brew cask install pandoc
-  brew cask install pd
-  # brew cask install pd-extended
-  brew cask install processing
-  brew cask install programmer-dvorak
-  brew cask install rstudio
-  brew cask install seil
-  # brew cask install slack
-  brew cask install steam
-  # brew cask install sublime-text
-  brew cask install telegram
-  brew cask install texstudio
-  brew cask install the-unarchiver
-  brew cask install transmission
-  brew cask install tuneup
-  brew cask install unetbootin
-  brew cask install vagrant
-  brew cask install virtualbox
-  brew cask install vlc
-
-fi
+brew install caskroom/cask/brew-cask
+# install homebrew casks
+brew cask install xquartz
+brew cask install acslogo
+# brew cask install alfred
+brew cask install atom
+brew cask install audacity
+brew cask install bit-slicer
+brew cask install filezilla
+# brew cask install ganttproject
+brew cask install gimp
+brew cask install github-desktop
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-hangouts
+brew cask install google-photos-backup
+brew cask install grandperspective
+brew cask install haskell-platform
+brew cask install inkscape
+brew cask install insomniax
+brew cask install iterm2
+brew cask install mactex
+# brew cask install macvim
+# brew cask install microsoft-office
+brew cask install minecraft
+# brew cask install mongohub
+brew cask install nethackcocoa
+brew cask install supercollider
+brew cask install pandoc
+brew cask install pd
+# brew cask install pd-extended
+brew cask install processing
+brew cask install programmer-dvorak
+brew cask install rstudio
+brew cask install seil
+# brew cask install slack
+brew cask install steam
+# brew cask install sublime-text
+brew cask install telegram
+brew cask install texstudio
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install tuneup
+brew cask install unetbootin
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
 
 exit 0
