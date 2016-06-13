b93700b0130b9ab0165cb6764c2eff17d260e502
Alberto Barradas <abcsds@gmail.com>
2016-06-13 00:30:49 -0500

    Add latex functionalities for atom

--- atom/install.sh
+++ atom/install.sh
@@ -1,5 +1,6 @@
 apm install atom-beautify
 apm install atom-css-comb
+apm install autocomplete-bibtex
 apm install autocomplete-python
 apm install autoclose-html
 apm install blame
@@ -12,13 +13,24 @@ apm install git-plus
 apm install hashrocket
 apm install jekyll
 apm install jslint
+apm install language-brainfuck
+apm install language-docker
+apm install language-julia
 apm install language-latex
+apm install language-pfm
 apm install language-prolog
 apm install language-r
 apm install language-vhdl
+# apm install latex
+apm install latex-autocomplete
+apm install latex-plus
+# apm install latextools
 apm install linter
 apm install linter-flake8
+apm install markdown-preview-plus
 apm install merge-conflicts
+apm install pdf-view
+apm install pandoc-convert
 apm install pigments
 apm install python-tools
 apm install set-syntax
