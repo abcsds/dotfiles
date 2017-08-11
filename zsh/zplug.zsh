7e9cd67c7b39d0a18c80bd06f5a5a9ac3879a56f
Alberto Barradas <abcsds@gmail.com>
2017-08-11 02:57:03 -0500

    Add zsh plugins

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -17,6 +17,12 @@ zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 # Alias-tips
 zplug "djui/alias-tips", from:github, defer:2
 
+# Use cd with tab
+zplug "changyuheng/zsh-interactive-cd", from:github, defer:2
+
+# Tmux session manager
+zplug "RobertAudi/tsm", from:github, defer:3
+
 # gitignore creation
 # use example: `gi emacs` 'gii emacs'
 zplug "voronkovich/gitignore.plugin.zsh", from:github, defer:3
@@ -38,13 +44,13 @@ zplug "zsh-users/zsh-history-substring-search"
 
 # bind UP and DOWN arrow keys
 # zmodload zsh/terminfo
-# bindkey "$terminfo[kcuu1]" history-substring-search-up
-# bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
 # bind UP and DOWN arrow keys (compatibility fallback
 # for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
-# bindkey "^[[A" history-substring-search-up
-# bindkey "^[[B" history-substring-search-down
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
 
 # bind P and N for EMACS mode
 # bindkey -M emacs "^P" history-substring-search-up
