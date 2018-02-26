ada15d13278ae568dee166c27ca6f985823d938f
Alberto Barradas <abcsds@gmail.com>
2018-02-27 00:37:15 +0100

    Update pip installs

--- atom/README.md
+++ atom/README.md
@@ -1,2 +1,4 @@
 # Atom
-The Atom text editor using [proton](https://github.com/dvcrn/proton) to configure atom. Installation requires then only `proton-mode`, and all config and keymaps are in the `proton.symlink` clojure file that gets soft linked to `~/.proton`.
+No proton, just plain atom. Proton is painfully slow.
+TODO:
+change keymaps back to vanilla atom.
--- python/install.sh
+++ python/install.sh
@@ -1,43 +1,94 @@
 if [ "$(uname -s)"=="Darwin" ]
 then
   # Python2
-  pip2 install --upgrade pip
-  pip2 install --upgrade setuptools
+  # pip2 install --upgrade pip
+  # pip2 install --upgrade setuptools
   # pip2 install ctypes --allow-external ctypes --allow-unverified ctypes
-  pip2 install pyobjc
-  pip2 install ipython
-  pip2 install virtualenv
-  pip2 install virtualenvwrapper
-  pip2 install autoenv
-  pip2 install numpy
-  pip2 install Pygments
+  # pip2 install pyobjc
+  # pip2 install ipython
+  # pip2 install virtualenv
+  # pip2 install virtualenvwrapper
+  # pip2 install autoenv
+  # pip2 install numpy
+  # pip2 install Pygments
   # pip2 install pygame
-  pip2 install simplecv
-  pip2 install matplotlib
+  # pip2 install simplecv
+  # pip2 install matplotlib
 
   # Python3
   pip3 install --upgrade pip
   pip3 install --upgrade setuptools
-  pip3 install pyobjc
+  pip3 install coursera-dl
+  pip3 install flake8
   pip3 install ipython
-  pip3 install numpy
-  pip3 install Pygments
-  pip3 install simplecv
+  pip3 install ipywidgets
+  pip3 install jupyter
+  pip3 install jupyter-contrib-nbextensions
+  pip3 install jupyter-tensorboard
   pip3 install matplotlib
-  pip3 install hg+http://bitbucket.org/pygame/pygame
-  # pip install pygame --allow-external pygame --allow-unverified pygame
-
-  # TODO:
-  # pip install turtle
-  # pip install --allow-external SC --allow-unverified SC SC
+  pip3 install mne
+  pip3 install nbconvert
+  pip3 install networkx
+  pip3 install nilearn
+  pip3 install nltk
+  pip3 install numpy
+  pip3 install pandas
+  pip3 install Pillow
+  pip3 install Pweave
+  pip3 install Pygments
+  pip3 install pyserial
+  pip3 install python-telegram-bot
+  pip3 install regex
+  pip3 install scikit-learn
+  pip3 install scikit-surprise
+  pip3 install scipy
+  pip3 install seaborn
+  pip3 install SimpleCV
+  pip3 install sklearn
+  pip3 install spyder
+  pip3 install sympy
+  pip3 install tensorflow
+  pip3 install tensorflow-tensorboard
+  pip3 install urllib3
+  pip3 install virtualenv
+  pip3 install virtualenvwrapper
+  pip3 install youtube-dl
 else
   pip install --upgrade pip
   pip install --upgrade setuptools
-  pip install Pygments
+  pip install coursera-dl
+  pip install flake8
   pip install ipython
+  pip install ipywidgets
+  pip install jupyter
+  pip install jupyter-contrib-nbextensions
+  pip install jupyter-tensorboard
   pip install matplotlib
+  pip install mne
+  pip install nbconvert
+  pip install networkx
+  pip install nilearn
+  pip install nltk
   pip install numpy
-  pip install simplecv
+  pip install pandas
+  pip install Pillow
+  pip install Pweave
+  pip install Pygments
+  pip install pyserial
+  pip install python-telegram-bot
+  pip install regex
+  pip install scikit-learn
+  pip install scikit-surprise
+  pip install scipy
+  pip install seaborn
+  pip install SimpleCV
+  pip install sklearn
+  pip install spyder
+  pip install sympy
+  pip install tensorflow
+  pip install tensorflow-tensorboard
+  pip install urllib3
   pip install virtualenv
   pip install virtualenvwrapper
+  pip install youtube-dl
 fi
