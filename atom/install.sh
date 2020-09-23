c88dac60c32fc01889fdc78d5c4518ebb6fae19f
Alberto Barradas <abcsds@gmail.com>
2020-09-23 14:59:10 +0200

    Update atom config

--- atom/install.sh
+++ atom/install.sh
@@ -1,34 +1,22 @@
 apm install atom-beautify
-# apm install atom-css-comb
 apm install autocomplete-bibtex
 apm install autocomplete-python
-# apm install autoclose-html
-# apm install blame
-# apm install busy-signal
 apm install city-lights-syntax
 apm install city-lights-ui
 apm install color-picker
-# apm install emmet
 # apm install figlet
 apm install file-icons
 apm install editor-stats
-# apm install git-log
-# apm install git-plus
+apm install git-log
 apm install git-time-machine
 apm install hydrogen
-# apm install intentions
 apm install jekyll
-# apm install jslint
-# apm install language-brainfuck
-# apm install language-common-lisp
 apm install language-csv
 # apm install language-docker
 # apm install language-emacs-lisp
 apm install language-julia
 apm install language-latex
 apm install language-matlab
-# apm install language-pfm
-# apm install language-prolog
 # apm install language-r
 # apm install language-vhdl
 apm install language-viml
@@ -49,7 +37,10 @@ apm install material-syntax
 # apm install pdf-view
 # apm install pandoc-convert
 apm install pigments
+# apm install proton
 apm install python-tools
+apm install python-black
 apm install set-syntax
-# apm install tidal
+apm install sort-words
+apm install split-diff
 apm install zotero-picker
