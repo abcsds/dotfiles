4daa9750d86c5658ac1a833020512ed50305f8f1
Alberto Barradas <abcsds@gmail.com>
2023-03-14 10:55:30 +0100

    Remove atom config

--- atom/README.md
+++
@@ -1,4 +0,0 @@
-# Atom
-No proton, just plain atom. Proton is painfully slow.
-
-https://github.com/mehcode/awesome-atom#git-time-machine
--- atom/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# Atom.io editor abbreviations
-function a () {
-  if [[ -n $1 ]]; then
-    atom "$1"
-  else
-    atom .
-  fi
-}
-alias apmup='apm update --no-confirm'
